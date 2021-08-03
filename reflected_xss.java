ec6aa3c78a71e66b567b5edbfa79ca04ec982fe2
Philipp Möhler <philipp.moehler@fujitsu.com>
2021-08-03 15:28:08 +0200

    burn

---
+++ reflected_xss.java
@@ -0,0 +1,7 @@
+public class RequestProcessor {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String name = req.getParameter("name");
+    PrintWriter out = resp.getWriter();
+    out.write("Hello " + name); // Noncompliant
+  }
+  }
