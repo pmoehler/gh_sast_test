5a4566326ef7cecd99a61d176f9c089fd5475e19
Philipp Möhler <philipp.moehler@fujitsu.com>
2021-08-04 14:04:44 +0200

    drop java

--- eval_injection.java
+++
@@ -1,9 +0,0 @@
-public class RequestProcessor {
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String input = req.getParameter("input");
-    
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName("JavaScript");
-        engine.eval(input); // Noncompliant
-  }
-}
